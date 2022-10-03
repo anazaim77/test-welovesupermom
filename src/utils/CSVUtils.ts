a844dcd0ab9c35cc2edb8bbc8aca3f9a7efb9416
Fajar Pamungkas <fajar.pamungkas@Flip-MacBook-M1.local>
2022-10-04 00:15:02 +0700

    :wrench: Change setup download csv

--- src/utils/CSVUtils.ts
+++ src/utils/CSVUtils.ts
@@ -1,21 +1,39 @@
 import { textUtils } from "./index";
-import RNFetchBlob from "react-native-blob-util";
-import { Platform } from "react-native";
 
-export const generateCSVFile = (text: string) => {
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
+import * as Permissions from "expo-permissions";
+import { jsonToCSV } from "react-native-csv";
+
+export const generateCSVFile = async (text: string) => {
   const csvTextValue = textUtils.convertStringToCSVString(text);
 
-  // write the current list of answers to a local csv file
-  const { dirs } = RNFetchBlob.fs;
-  const dirToSave = Platform.OS === "ios" ? dirs.DocumentDir : dirs.DownloadDir;
-  const pathToWrite = `${dirToSave}/data.csv`;
-  console.log("pathToWrite", pathToWrite);
-  // pathToWrite /storage/emulated/0/Download/data.csv
-  RNFetchBlob.fs
-    .writeFile(pathToWrite, csvTextValue, "utf8")
-    .then(() => {
-      console.log(`wrote file ${pathToWrite}`);
-      // wrote file /storage/emulated/0/Download/data.csv
-    })
-    .catch((error) => console.error(error));
+  const CSV = jsonToCSV(csvTextValue);
+
+  // Name the File
+  const directoryUri = FileSystem.documentDirectory;
+  const fileUri = directoryUri + `formData.csv`;
+
+  // Ask permission (if not granted)
+  const perm = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+  if (perm.status != "granted") {
+    console.log("Permission not Granted!");
+    return;
+  }
+
+  // Write the file to system
+  FileSystem.writeAsStringAsync(fileUri, CSV);
+
+  try {
+    const asset = await MediaLibrary.createAssetAsync(fileUri);
+    const album = await MediaLibrary.getAlbumAsync("forms");
+    console.log(album);
+    if (album == null) {
+      await MediaLibrary.createAlbumAsync("forms", asset, true);
+    } else {
+      await MediaLibrary.addAssetsToAlbumAsync([asset], album, true);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
